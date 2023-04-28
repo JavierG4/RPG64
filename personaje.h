47674e36d819bc629447e85c1231f591925e8934
usuario <usuario@IB-Javi.G>
2023-04-28 09:21:34 +0000

    e

---
+++ personaje.h
@@ -0,0 +1,42 @@
+/*
+AUTORES :Javier González Brito .
+ULT FECHA: 17/04/2023
+Clase Personaje
+COMENTARIOS: Clase principal del personaje
+*/
+#ifndef PERSONAJE_H_
+#define PERSONAJE_H_
+
+#include <iostream>
+#include <cctype>
+
+template <class T> class personaje_t {
+ public:
+ 
+
+
+
+
+
+
+
+
+
+
+
+ private:
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+#endif  // PERSONAJE_H_
