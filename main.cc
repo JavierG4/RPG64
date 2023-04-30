93e254a6a8685d22163bd26422901350c9545e76
JoseGHdezC <114774544+JoseGHdezC@users.noreply.github.com>
2023-04-30 11:57:01 +0100

    e

--- main.cc
+++ main.cc
@@ -11,22 +11,6 @@ Programa Principal
 COMENTARIOS: 
 */
 # include <iostream>
-# include <cstdio>
-# include <cstlib>
 
-void Suma() {
-  int numero1, numero2, resultado;
-  std::cout << "Introduzca 1: ";
-  std::cin >> numero1;
-  std::cout << "\nIntroduzca 2: ";
-  std::cin >> numero2;
-
-  resultado = numero1 + numero2;
-
-  std::cout << "\nLa suma total es: " << resultado << std::endl;
-}
 int main() {
-  
-  Suma();
-  return 0;
 }
