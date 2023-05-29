d93f05519d09fb6f236473aff92fcb845f2aafb0
usuario <alu0101562445@ull.edu.es>
2023-05-29 14:35:19 +0000

    actualización

--- mapa_implementacion/main_maze_t.cpp
+++ mapa_implementacion/main_maze_t.cpp
@@ -20,36 +20,42 @@
 
 using namespace std;
 
-void printWithDelay(const std::string& s, int delay = 50000) {
+void printWithDelay(const std::string& s, int delay = 20000) {
+  std::cout << "\033[1;31m";
   for (char c : s) {
     std::cout << c;
     std::flush(std::cout); // asegúrate de que se imprima inmediatamente
     usleep(delay); // espera el tiempo de retraso especificado
   }
+  std::cout << "\033[0m";
 }
 
 int main() {
   system("clear"); 
   std::string p = "Habia una vez una pobre niña que quería tu preciado helado. \nIntentó arrancártelo de la mano, pero rapidamente tu pudiste esquivarla con gran perspicacia.\nFinalmente te caiste por el balanceo que te proporcionó aquella maniobra evasiva\n";
   printWithDelay(p);
+  p = "Ahora tienes la decisión, que querrá hacer el protagonista?\n(m) mostrar el mapa\n(s) ir al sur\n";
+  printWithDelay(p);
+  char eleccion;
+  std::cin >> eleccion;
+  if (eleccion == 'm') {
+    maze_t M;
+    // ifstream is used for reading files
+    // We'll read from a file called data_maze_1.txt
+    std::ifstream inf{ "mapa.txt" };
 
-  maze_t M;
-  // ifstream is used for reading files
-  // We'll read from a file called data_maze_1.txt
-  std::ifstream inf{ "mapa.txt" };
-
-  // If we couldn't open the input file stream for reading
-  if (!inf) {   
-    // Print an error and exit
-    std::cout << "Uh oh, Sample.txt could not be opened for reading!\n";
-    return 1;
+    // If we couldn't open the input file stream for reading
+    if (!inf) {   
+      // Print an error and exit
+      std::cout << "Uh oh, Sample.txt could not be opened for reading!\n";
+      return 1;
+    }
+    // lee el mapa del inf
+    inf >> M;
+    // se hace un clear y se imprime el mapa.
+    system("clear"); 
+    std::cout << M << endl;
   }
-  // lee el mapa del inf
-  inf >> M;
-  // se hace un clear y se imprime el mapa.
-  system("clear"); 
-  std::cout << M << endl;
-
   return 0;
 }
 
