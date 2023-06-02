669d55cb09e9ca5fac9a5dd4e41d4691401aba35
Javi G <alu0101548197@ull.edu.es>
2023-06-03 00:14:05 +0100

    actualización

--- codigo/Libreriacc/main.cc
+++ codigo/Libreriacc/main.cc
@@ -10,10 +10,13 @@ ULT FECHA: 17/04/2023
 Programa Principal
 COMENTARIOS: 
 */
-//#include "libreria.h"
+#include "libreria.h"
 #include <iostream>
 
 int main() {
     //system("clear");
+    
     std::cout << "_____________________________________________________________________________________________" << std::endl;
+    std::cout << "___  ___  ___  ___" << "       ___  ___  ___  ___" << "       ___  ___  ___  ___             |" << std::endl;
+    std::cout << "|                                                                                    |" << std::endl;
 }
