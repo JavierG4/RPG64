f461105dd21b82a794ba34e9079372c1d0f4f0a0
Javi G <alu0101548197@ull.edu.es>
2023-06-03 02:08:52 +0100

    actualización

--- codigo/Libreriacc/main.cc
+++ codigo/Libreriacc/main.cc
@@ -17,7 +17,13 @@ int main() {
     system("clear");
 
         //elección de personajes
-        
+    std::cout << "------------------------------------ELECCION DE PERSONAJE------------------------------------" << std::endl;
+    std::cout << "\n";
+    Atributos personaje1(50,190,90,true); // Mediana edad (50), Alto, 90 kilos, Tio
+    Atributos personaje2(20,160,60,false); // Edad joven (20), Baja, 60 kilos, Tia  
+    Atributos personaje3(70,170,70, false); // Mediana edad (70), media, 70 kilos, Tia  
+    Atributos personaje4(100,250,150,true); // Mediana edad (100), Alto de cojones, 150 kilos, Tio
+    Estadistica      
     std::cout << "_____________________________________________________________________________________________" << std::endl;
     std::cout << "|     ___  ___  ___    " << "      ___  ___  ___    " << "      ___  ___  ___          ___  ___  ___     |" << std::endl;
     //std::cout << "|    |             |        |             |        |             |        |             |    |" << std::endl;
