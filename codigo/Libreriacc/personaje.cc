fd385a58980a18ca5c125b985263ef01e5ddf099
Javi G <alu0101548197@ull.edu.es>
2023-06-03 01:32:54 +0100

    mejora

--- codigo/Libreriacc/personaje.cc
+++ codigo/Libreriacc/personaje.cc
@@ -15,5 +15,11 @@ COMENTARIOS: Clase principal del personaje
 Personaje::Personaje(Atributos atributos, Estadisctica estadisctica) {
   atributos_ = atributos;
   estadisticas_ = estadisticas;
-  
+  mochila_.set_capacidad(15);
+  mochila_.set_cantidad(0);
+  salud_.set_cabeza(100);
+  salud_.set_piernas(100);
+  salud_.set_interior(100);
+  salud_.set_torso(100);
+  salud_.set_brazos(100);
   }
