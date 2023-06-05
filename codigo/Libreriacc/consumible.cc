aed336514b974421370525d2a1b7cb531b16fcb3
Javier Gomez Alayon <alu0101562445@ull.edu.es>
2023-06-06 00:12:36 +0100

    actualización

--- codigo/Libreriacc/consumible.cc
+++ codigo/Libreriacc/consumible.cc
@@ -13,6 +13,6 @@ COMENTARIOS: Aquí estarán la clase cosumible
 */
 #include "../Libreriah/libreria.h"
 
-//std::string Consumible::get_nombre_con(void) const {
-//  return nombre_;
-//}
+std::string Consumible::get_nombre_con(void) const {
+  return nombre_;
+}
