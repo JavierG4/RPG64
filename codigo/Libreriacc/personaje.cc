082ac57f2bd071efa171b6c9dded3debd45d5e24
Javier Gomez Alayon <alu0101562445@ull.edu.es>
2023-06-06 00:04:34 +0100

    actualización

--- codigo/Libreriacc/personaje.cc
+++ codigo/Libreriacc/personaje.cc
@@ -10,19 +10,7 @@ ULT FECHA: 17/04/2023
 Clase Personaje
 COMENTARIOS: Clase principal del personaje
 */
-#include <iostream>
-#include "../Libreriah/consumible.h"
-#include "objetos.h"
-#include "dll_node_t.h"
-#include "dll_t.h"
-#include "mochila.h"
-#include "salud.h"
-#include "atributos.h"
-#include "estadistica.h"
-#include "personaje.h"
-#include <cassert>
-#include <string>
-#include "maze_t.hpp"
+#include "../Libreriah/libreria.h"
 
 template<class T> Personaje<T>::Personaje(Atributos atributos, Estadistica estadistica) {
   atributos_ = atributos;
